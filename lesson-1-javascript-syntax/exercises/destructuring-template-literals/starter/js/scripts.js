ee417ea4ed1530058decdad6045dbd70bc32551a
doomhesoka55 <ahmadjaffal2003@gmail.com>
2025-10-03 11:58:39 +0300

    Update scripts.js

--- lesson-1-javascript-syntax/exercises/destructuring-template-literals/starter/js/scripts.js
+++ lesson-1-javascript-syntax/exercises/destructuring-template-literals/starter/js/scripts.js
@@ -9,10 +9,12 @@ const user = {
 
 const destructureExercise = async() => {
     // This is where your destructuring should take place. destructure the user object provided and then use template literals to create the name and bio. If you destructure the user object completely you won't need to worry about the profilePic but it will show up on the page.
+     const {firstName, lastName, city, state, favorites, profilePic} = user
+     const [fave1, fave2] = favorites
 
     // use template literals here with your destructured object
-    const name = 'EDIT THIS'
-    const bio  = 'EDIT THIS'
+    const name =  `${firstName} ${lastName}`
+    const bio  = `Hi! My name is ${name} and I'm from ${city}, ${state}. Some of my favorite things are ${fave1} and ${fave2}.`
 
     // DO NOT EDIT BELOW THIS LINE!
 
